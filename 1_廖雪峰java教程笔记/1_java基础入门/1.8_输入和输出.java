609c91decce181a9eada0ae22c868d66de09fb3e
timothy <wangsz2015@foxmail.com>
2021-03-03 21:45:11 +0800

    some basic nots of java

---
+++ 1_廖雪峰java教程笔记/1_java基础入门/1.8_输入和输出.java
@@ -0,0 +1,29 @@
+
+// 导入Scanner类
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        // 输入
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("input a integer: ");
+        // int n = sc.nextInt();  //读取一行输入并获取整数
+        System.out.println("input your name: ");
+        // --- 注意：如果上面9，10两行不注释，13行无法正常执行，好像是接收了默认上面的换行符
+        String name = sc.nextLine(); // 读取一行输入并获取字符串
+        System.out.println("my name: " + name);
+        // 输出
+        System.out.print("A,");
+        System.out.print("B,"); // 在同一行连续输出
+        System.out.print("C.");
+        System.out.println();   //没有输出,为空
+        // System.out.println(n);
+        System.out.println("END"); // 另起一行输出
+        double d = 12900000;
+        System.out.println(d); // 1.29E7 科学计数法
+        //格式化输出
+        double pi = 3.1415926;
+        System.out.printf("%.2f\n", pi); //显示2位小数3.14
+        System.out.printf("%.4f\n", pi); //显示4位小数3.1416（四舍五入）
+    }
+}
