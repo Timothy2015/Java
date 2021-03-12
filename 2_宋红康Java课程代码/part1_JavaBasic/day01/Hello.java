06ca42629d2e30fe2965e1b65adb1f00d11543b4
timothy <wangsz2015@foxmail.com>
2021-03-12 18:18:17 +0800

    a little changes

--- 2_宋红康Java课程代码/part1_JavaBasic/day01/Hello.java
+++ 2_宋红康Java课程代码/part1_JavaBasic/day01/Hello.java
@@ -1,29 +1,31 @@
 /*
-对�??一个java程序进�?�总结
-1. java程序编写-编译-运�?�的过程
-编写：我�??将编写的java代码保存在以".java"结尾的源文件�??
+对第一个java程序进行总结
+1. java程序编写-编译-运行的过程
+编写：我们将编写的java代码保存在以".java"结尾的源文件中
 编译：使用javac.exe命令编译我们的java源文件。格式：javac 源文件名.java
-运�?�：使用java.exe命令解释运�?�我�??的字节码文件�?? 格式：java 类名
+运行：使用java.exe命令解释运行我们的字节码文件。 格式：java 类名
 
-2. 一�??源文件中，只有一�??类声明为public
-* 在一个java源文件中�??以声明�?�个class。但�??，只能最多有一�??类声明为public的�?
-而且要求声明为public的类的类名必须与源文件名相同�??
+2. 一个类中只能有一个public类
+在一个java源文件中可以声明多个class。但是，只能最多有一个类声明为public的。
+而且要求声明为public的类的类名必须与源文件名相同。
 
-3. 程序的入口是main()方法。格式是固定的�?
+3. 程序的入口是main()方法。格式是固定的。
 
-4. 输出�??句：
-System.out.println():先输出数�??，然后换�??
-System.out.print():�??输出数据
+4. 输出语句：
+System.out.println():先输出数据，然后换行
+System.out.print():只输出数据
 
-5.每一行执行�??句都�??";"结束�??
+5.每一行执行语句都以";"结束。
 
-6.编译的过程：编译以后，会生成一�??或�?�个字节码文件。字节码文件的文件名与java源文件中的类名相同�?
+6.编译的过程：
+	* 编译以后，会生成“一个或多个”字节码文件。字节码文件的文件名与java源文件中的类名相同。
 
 */
+
 public class Hello {
-	public static void main(String[] args) {//public static void main(String a[]) {//arguments:参数
+	public static void main(String[] args) {//public static void main(String a[]) {//arguments:����
 		System.out.print("Hello World!");
-		System.out.println();//�?�???
+		System.out.println();//换行
 		System.out.println("Hello World!");
 		}
 }
--- 2_宋红康Java课程代码/part1_JavaBasic/day01/HelloWorld.java
+++ 2_宋红康Java课程代码/part1_JavaBasic/day01/HelloWorld.java
@@ -1,5 +1,4 @@
 class HelloChina{
-
 	public static void main(String[] args){
 		System.out.println("Hello,World!");
 	}
